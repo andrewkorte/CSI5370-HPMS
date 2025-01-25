5f2f247781861ad99013c275b1bd61854708acbe
Andrew <andrewkorte@oakland.edu>
2025-01-25 13:31:25 -0500

    CSI5370 - First Commit

---
+++ src/main/java/com/CSI5370/HomePurchaseManagementSystem/HomePurchaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CSI5370.HomePurchaseManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePurchaseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomePurchaseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CSI5370/HomePurchaseManagementSystem/HomePurchaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSI5370.HomePurchaseManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePurchaseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
