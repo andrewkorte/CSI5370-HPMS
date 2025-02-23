0f37c837dd2a5de1451e5e660534c266273e0964
Andrew <andrewkorte@oakland.edu>
2025-02-23 14:12:18 -0500

    Made create endpoint for customer

--- src/main/java/com/CSI5370/HomePurchaseManagementSystem/HPMSEndpoints.java
+++ src/main/java/com/CSI5370/HomePurchaseManagementSystem/HPMSEndpoints.java
@@ -1,20 +1,30 @@
 package com.CSI5370.HomePurchaseManagementSystem;
 
 
+import jakarta.validation.constraints.Pattern;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HPMSEndpoints {
 
     //localhost:8080/search
 
-    @GetMapping("/search/{accountNumber}")
-    public ResponseEntity<Integer> someMethod(@PathVariable @Pattern(regexp = "fdasdf") String accountNumber,
-                                              @RequestParam){
-        //do stuff
+    @PostMapping("/customer/create")
+    public ResponseEntity<Integer> createCustomer(@RequestBody @Pattern(regexp = "readf") String firstName,
+                                                  @RequestBody @Pattern(regexp = "readfds") String lastName,
+                                                  @RequestBody @Pattern(regexp = "fdasgd") String ssn,
+                                                  @RequestBody @Pattern(regexp = "fdasfa") int income){
+
+        return null;
     }
+
+
+//    @GetMapping("/search/{accountNumber}")
+//    public ResponseEntity<Integer> someMethod(@PathVariable @Pattern(regexp = "fdasdf") String accountNumber,
+//                                              @RequestParam @Pattern(regexp = "reasdg") Integer heresAnInt,
+//                                              @RequestBody @Pattern(regexp = "fdafsa") Boolean heresABool){
+//        //do stuff
+//    }
 }
