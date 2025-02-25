a2bf99cbf404e54d6678cd628cef4df4d0a1c277
Andrew <andrewkorte@oakland.edu>
2025-02-24 19:29:24 -0500

    Created customer create method

---
+++ src/main/java/com/CSI5370/HomePurchaseManagementSystem/Services/CustomerService.java
@@ -0,0 +1,49 @@
+package com.CSI5370.HomePurchaseManagementSystem.Services;
+
+import java.sql.*;
+
+public class CustomerService {
+
+    String URL = "localhost:5432";
+
+    String USER = "csi5370";
+
+    String PASSWORD = "mypassword";
+
+    public int createCustomer(String firstName, String lastName, String ssn, float income) throws SQLException {
+        Connection conn = null;
+
+        String createSQL = "INSERT INTO customer (firstName, lastName, ssn, income) values (?, ?, ?, ?) returning id;";
+
+        int custId = 0;
+
+        try(PreparedStatement create = conn.prepareStatement(createSQL)){
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            create.setString(1,firstName);
+            create.setString(2,lastName);
+            create.setString(3,ssn);
+            create.setFloat(4, income);
+
+            ResultSet rs = create.executeQuery();
+
+            while (rs.next()){
+                custId = rs.getInt("id");
+            }
+
+        }catch (SQLException e){
+            System.err.println("JDBC Driver not found!");
+            e.printStackTrace();
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("Connection closed.");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return custId;
+    }
+}
