eb8530bb671e4eca702f7ddc93fd89fbfb20b322
Thegreenmoray <112501898+Thegreenmoray@users.noreply.github.com>
2025-02-24 19:42:33 -0500

    added Realtor and home

--- src/main/java/com/CSI5370/HomePurchaseManagementSystem/Services/CustomerService.java
+++ src/main/java/com/CSI5370/HomePurchaseManagementSystem/Services/CustomerService.java
@@ -1,7 +1,9 @@
 package com.CSI5370.HomePurchaseManagementSystem.Services;
 
-import java.sql.*;
+import org.springframework.stereotype.Service;
 
+import java.sql.*;
+@Service
 public class CustomerService {
 
     String URL = "localhost:5432";
@@ -46,4 +48,84 @@ public class CustomerService {
         }
         return custId;
     }
+
+
+    public int createRealtor(int employeenum,String firstName, String lastName, float commissionRate) throws SQLException {
+        Connection conn = null;
+
+        String createSQL = "INSERT INTO Realtor (employeenum, firstName, lastName, commissionRate) values (?, ?, ?, ?) returning id;";
+
+        int realId = 0;
+
+        try(PreparedStatement create = conn.prepareStatement(createSQL)){
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            create.setInt(1,employeenum);
+            create.setString(2,firstName);
+            create.setString(3,lastName);
+            create.setFloat(4, commissionRate);
+
+            ResultSet rs = create.executeQuery();
+
+            while (rs.next()){
+                realId = rs.getInt("id");
+            }
+
+        }catch (SQLException e){
+            System.err.println("JDBC Driver not found!");
+            e.printStackTrace();
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("Connection closed.");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return realId;
+    }
+
+
+    public int createHome(int streetNum, String city, String state, float price, int squareFeet) throws SQLException {
+        Connection conn = null;
+
+        String createSQL = "INSERT INTO Home (streetNum, city, state, price, squareFeet) values (?, ?, ?, ?) returning id;";
+
+        int homeId = 0;
+
+        try(PreparedStatement create = conn.prepareStatement(createSQL)){
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            create.setInt(1,streetNum);
+            create.setString(2,city);
+            create.setString(3,state);
+            create.setFloat(4, price);
+            create.setInt(5,squareFeet);
+
+            ResultSet rs = create.executeQuery();
+
+            while (rs.next()){
+                homeId = rs.getInt("id");
+            }
+
+        }catch (SQLException e){
+            System.err.println("JDBC Driver not found!");
+            e.printStackTrace();
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("Connection closed.");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return homeId;
+    }
+
+
+
 }
