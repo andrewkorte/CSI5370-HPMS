b89b45a44e82d320dd70317a3ce3a384c52aaabb
Thegreenmoray <112501898+Thegreenmoray@users.noreply.github.com>
2025-03-01 16:03:54 -0500

    new

--- src/test/java/com/CSI5370/HomePurchaseManagementSystem/HPMSEndpointsTest.java
+++ src/test/java/com/CSI5370/HomePurchaseManagementSystem/HPMSEndpointsTest.java
@@ -1,7 +1,9 @@
 package com.CSI5370.HomePurchaseManagementSystem;
 
 import com.CSI5370.HomePurchaseManagementSystem.Domain.Customer;
+import com.CSI5370.HomePurchaseManagementSystem.Domain.Home;
 import com.CSI5370.HomePurchaseManagementSystem.Services.CustomerService;
+import com.CSI5370.HomePurchaseManagementSystem.Services.HomeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,6 +25,9 @@ class HPMSEndpointsTest {
 
     @Mock
     CustomerService customerService;
+
+     @Mock
+    HomeService homeService;
 
     @Test
     public void createCustomer_Returns200WithId() throws SQLException {
@@ -61,4 +66,52 @@ class HPMSEndpointsTest {
         verify(customerService, times(1)).deleteCustomer(1);
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
+
+  /* ----------------------------------------------------------------------------------------------------------*/
+
+
+    @Test
+    public void createHome_Returns200WithId() throws SQLException {
+
+        when(homeService.createHome(5046, "Hell", "Michigan", 40000.00F,2600)).thenReturn(1);
+
+        ResponseEntity<Integer> result = hpmsEndpoints.createHome(5046, "Hell", "Michigan", 40000.00F,2600);
+
+        assertThat(result.getBody()).isEqualTo(1);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void getHome_Returns200WithCustomer() {
+        Home home = new Home(1,5046, "Hell", "Michigan", 40000.00F,2600);
+
+        when(homeService.gethome(1)).thenReturn(home);
+
+        ResponseEntity<Home> result = hpmsEndpoints.getHome(1);
+
+        assertThat(result.getBody().getId()).isEqualTo(1);
+        assertThat(result.getBody().getAddress()).isEqualTo(5046);
+        assertThat(result.getBody().getCity()).isEqualTo("Hell");
+        assertThat(result.getBody().getState()).isEqualTo("Michigan");
+        assertThat(result.getBody().getPrice()).isEqualTo(40000.00F);
+        assertThat(result.getBody().getSquareFeet()).isEqualTo(2600);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void deleteHome_Returns204() {
+
+        doNothing().when(homeService).deleteHome(1);
+
+        ResponseEntity<Void> result = hpmsEndpoints.deletehome(1);
+
+        verify(customerService, times(1)).deleteCustomer(1);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    /* ----------------------------------------------------------------------------------------------------------*/
+
+
+
+
 }
