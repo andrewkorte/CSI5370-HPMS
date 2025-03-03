07d107964ea8f31820e8d94b5438f8fe1ebbd8b0
Andrew <andrewkorte@oakland.edu>
2025-03-03 18:38:57 -0500

    Add validation to customer endpoint

--- src/main/java/com/CSI5370/HomePurchaseManagementSystem/HPMSEndpoints.java
+++ src/main/java/com/CSI5370/HomePurchaseManagementSystem/HPMSEndpoints.java
@@ -14,6 +14,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -45,10 +48,13 @@ public class HPMSEndpoints {
     @Operation(summary = "Create a customer record", description = "Create a customer record")
     @ApiResponse(responseCode = "200", description = "Resource Created")
     @ApiResponse(responseCode = "503", description = "Service Unavailable", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
-    public ResponseEntity<Integer> createCustomer(@RequestParam @Pattern(regexp = "[a-zA-Z]+") String firstName,
-                                                  @RequestParam @Pattern(regexp = "[a-zA-Z]+") String lastName,
-                                                  @RequestParam @Pattern(regexp = "[0-9]{2}-[0-9]{3}-[0-9]{4}") String ssn,
-                                                  @RequestParam float income) throws SQLException {
+    public ResponseEntity<Integer> createCustomer(@Valid @RequestParam @Pattern(regexp = "[a-zA-Z]+") @Schema(description = "First name containing only letters", pattern = "[a-zA-Z]+") String firstName,
+                                                  @Valid @RequestParam @Pattern(regexp = "[a-zA-Z]+") @Schema(description = "Last name containing only letters", pattern = "[a-zA-Z]+") String lastName,
+                                                  @Valid @RequestParam @Pattern(regexp = "[0-9]{2}-[0-9]{3}-[0-9]{4}") @Schema(description = "Social Security Number format XX-XXX-XXXX", pattern = "[0-9]{2}-[0-9]{3}-[0-9]{4}") String ssn,
+                                                  @Valid @RequestParam @Schema(description = "Annual income in float format (min: 0, max: 1,0000,000)", minimum = "0", maximum = "10000000")
+                                                  @DecimalMin(value = "0.0", message = "Income must be at least 1000")
+                                                  @DecimalMax(value = "10000000.0", message = "Income must not exceed 1,0000,000")
+                                                  float income) throws SQLException {
         int custId = customerService.createCustomer(firstName, lastName, ssn, income);
         return ResponseEntity.ok(custId);
     }
@@ -58,7 +64,7 @@ public class HPMSEndpoints {
     @ApiResponse(responseCode = "200", description = "Resource Created")
     @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
     @ApiResponse(responseCode = "503", description = "Service Unavailable", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
-    public ResponseEntity<Customer> getCustomer(@PathVariable int customerid){
+    public ResponseEntity<Customer> getCustomer(@Valid @PathVariable int customerid){
 
         Customer customer = customerService.getCustomer(customerid);
         return ResponseEntity.ok(customer);
@@ -69,7 +75,7 @@ public class HPMSEndpoints {
     @ApiResponse(responseCode = "204", description = "Resource Deleted")
     @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
     @ApiResponse(responseCode = "503", description = "Service Unavailable", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
-    public ResponseEntity<Void> deleteCustomer(@PathVariable int customerid){
+    public ResponseEntity<Void> deleteCustomer(@Valid @PathVariable int customerid){
 
         customerService.deleteCustomer(customerid);
 
@@ -158,10 +164,11 @@ public class HPMSEndpoints {
 
     @PostMapping("home/create")
     @ApiResponse(responseCode = "503", description = "Service Unavailable", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
-    public ResponseEntity<Integer> createHome(@RequestParam int address, @RequestParam String street,
-                                                @RequestParam @Pattern(regexp = "[a-zA-Z]+") String city,
-                                                 @RequestParam @Pattern(regexp = "[a-zA-Z]+") String state,
-                                                 @RequestParam float price,
+    public ResponseEntity<Integer> createHome(@RequestParam int address,
+                                              @RequestParam String street,
+                                              @RequestParam @Pattern(regexp = "[a-zA-Z]+") String city,
+                                              @RequestParam @Pattern(regexp = "[a-zA-Z]+") String state,
+                                              @RequestParam float price,
                                               @RequestParam int squareFeet) throws SQLException {
        int homeid =homeService.createHome(address, street,city,state,price,squareFeet);
         return ResponseEntity.ok(homeid);
@@ -189,9 +196,5 @@ public class HPMSEndpoints {
 
         return ResponseEntity.noContent().build();
     }
-
-
-
-
 
 }
